5993686431208b2d21e47517f20eae410eed3c23
Srija Rayikanti <srijarayikanti@gmail.com>
2026-06-07 19:44:03 +0530

    Initial commit

---
+++ src/main/java/com/example/cafe_online/CafeOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.cafe_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cafe_online/CafeOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafe_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
